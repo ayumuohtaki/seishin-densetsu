57e5c434ea2a37acb925a931477ccfc303da5493
ayumuohtaki <ayumu0821@gmail.com>
2023-02-10 15:07:44 +0900

    companyのcssを変更。

--- page-business.php
+++ page-business.php
@@ -4,8 +4,8 @@
                   <ul class="commons">
 <?php
 $common_pages = get_child_pages();
-if ( $common_pages->have_posts() ) :
-    while( $common_pages->have_posts() ) : $common_pages->the_post();
+if ( $common_pages->have_posts() ):
+    while( $common_pages->have_posts() ): $common_pages->the_post();
         get_template_part( 'content-common' );
     endwhile;
     wp_reset_postdata();
